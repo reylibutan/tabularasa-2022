07e99b963ae71087c9047bc731b6829a369fffd5
reylibutan <reylibutan@gmail.com>
2022-05-07 00:46:51 +0800

    Initial commit

---
+++ src/test/java/com/libutan/tabularasa/TabularasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libutan.tabularasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabularasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
